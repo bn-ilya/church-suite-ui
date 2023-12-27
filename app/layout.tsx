946984e6ced3f4682a5b40275dcfe2a976f6f5b3
bn-ilya <bn.ilyaa@yandex.ru>
2023-12-27 08:23:40 +0300

    add theme switcher in header

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} min-h-screen`}>
+      <body suppressHydrationWarning={true} className={`${inter.className} min-h-screen`}>
         <Providers>
           <div className='min-h-screen flex flex-col justify-between'>
             <Header/>
--- components/Header.tsx
+++ components/Header.tsx
@@ -3,29 +3,25 @@ import Link from "next/link";
 import { FC } from 'react';
 import NavBarLinks from "./NavBarLinks/NavBarLinks";
 import { linksList } from "@/static-data/linksList/linksList";
+import { ThemeSwitcherBtn } from './ThemeSwitcherBtn/ThemeSwitcherBtn';
 
 const Header: FC = () => {
   return (
     <header>
-    <Navbar position="static">
-      <NavbarBrand>
-        <div>logo</div>
-        <p className="font-bold text-inherit">ACME</p>
-      </NavbarBrand>
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavBarLinks linksList={linksList} />
-      </NavbarContent>
-      <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Button as={Link} color="primary" href="#" variant="flat">
-            Sign Up
-          </Button>
-        </NavbarItem>
-      </NavbarContent>
-    </Navbar>
+      <Navbar position="static">
+        <NavbarBrand>
+          <div>logo</div>
+          <p className="font-bold text-inherit">ACME</p>
+        </NavbarBrand>
+        <NavbarContent className="hidden sm:flex gap-4" justify="center">
+          <NavBarLinks linksList={linksList} />
+        </NavbarContent>
+        <NavbarContent justify="end">
+          <NavbarItem>
+            <ThemeSwitcherBtn />
+          </NavbarItem>
+        </NavbarContent>
+      </Navbar>
     </header>
   )
 };
