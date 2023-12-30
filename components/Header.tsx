3e4c866ae3f75b3f680562b25da3a2a39e5f13d7
bn-ilya <bn.ilyaa@yandex.ru>
2023-12-30 17:01:19 +0300

    paint logo in header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,23 +1,17 @@
-import {  Link, Navbar,   NavbarBrand,   NavbarContent, NavbarItem, NavbarMenu, NavbarMenuItem, NavbarMenuToggle } from "@nextui-org/react";
+import {  Navbar,   NavbarBrand,   NavbarContent, NavbarItem, NavbarMenu, NavbarMenuItem, NavbarMenuToggle } from "@nextui-org/react";
 import { FC } from 'react';
-import Image from 'next/image';
-import LogoChurch from '../public/logoChurch/logoChurch.svg';
 import NavBarLinks from "./NavBarLinks/NavBarLinks";
 import { linksList } from "@/static-data/linksList/linksList";
 import { ThemeSwitcherBtn } from './ThemeSwitcherBtn/ThemeSwitcherBtn';
 import { headerHight } from "@/utils/constants";
 import { NavBarMobileMenu } from "./NavBarMobileMenu/NavBarMobileMenu";
+import { LogoChurch } from "./LogoChurch/LogoChurch";
 
 const Header: FC = () => {
   return (
       <Navbar maxWidth="full" height={`${headerHight}px`} isBordered className="fixed">
         <NavbarBrand>
-          <Image
-              priority
-              src={LogoChurch}
-              className='w-[20px] h-auto'
-              alt="Логотип церкви"
-           />
+          <LogoChurch className='w-[20px] h-auto text-foreground' />
         </NavbarBrand>
         <NavbarContent className="hidden sm:flex gap-4" justify="center">
           <NavBarLinks linksList={linksList} />
