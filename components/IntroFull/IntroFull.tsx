f38350b33a95d073afcab69f039d052d7975af3c
bn-ilya <bn.ilyaa@yandex.ru>
2024-01-03 12:33:59 +0300

    adapted intro for mobile

--- components/IntroFull/IntroFull.tsx
+++ components/IntroFull/IntroFull.tsx
@@ -5,13 +5,13 @@ const unbounded = Unbounded({ subsets: ['cyrillic'] });
 
 export const IntroFull = () => {
   return (
-    <div className={`h-screen w-full relative`}>
+    <div className={`h-dvh sm:h-screen w-full relative`}>
       <div className="max-w-7xl mx-auto h-full flex  flex-col gap-4 justify-center items-center">
-        <div className='flex gap-8'>
-          <LogoChurch className='w-[80px] h-auto text-white' />
-          <h1 className={`${unbounded.className} text-[54px] font-bold leading-[2] text-white`}>Церковь Кропоткина</h1>
+        <div className='flex gap-4 sm:gap-8 px-6 items-center'>
+          <LogoChurch className='w-[55px] lg:w-[80px] h-auto text-white' />
+          <h1 className={`${unbounded.className} text-[26px] sm:text-[36px] lg:text-[54px] font-bold w-min sm:w-auto leading-[1] text-white`}>Церковь Кропоткина</h1>
         </div>
-        <p className='max-w-[800px] text-white text-center text-lg'>Мы рады, что вы посетили наш сайт. Это удобный способ познакомиться с нашей церковью, во что мы верим и чем занимаемся. Будьте как дома</p>
+        <p className='max-w-[600px] lg:max-w-[800px] text-white text-center text-sm sm:text-base lg:text-lg px-4'>Доборо пожаловать, будьте как дома</p>
       </div>
       <video muted={true} autoPlay={true} loop={true} className="absolute top-0 left-0 min-w-full min-h-full object-cover z-[-1]">
         <source src='/video/intro-video.mp4' type="video/mp4"/>
