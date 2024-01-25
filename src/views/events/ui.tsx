bbc8f0ed25cbe5608ba34137912a50972b5f76ff
bn-ilya <bn.ilyaa@yandex.ru>
2024-01-25 17:10:25 +0300

    refactor: eventsIntro move to widgets layer

---
+++ src/views/events/ui.tsx
@@ -0,0 +1,11 @@
+import { MainAbout } from "@/src/widgets/main-about";
+import { EventsIntro } from "@/src/widgets/events-intro";
+
+export const Events = () => {
+  return (
+    <>
+      <EventsIntro/>
+      <MainAbout/>
+    </> 
+  )
+}
