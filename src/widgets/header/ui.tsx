60bae33ab83b9d5a588a211c69c5c47cb58a8a67
bn-ilya <bn.ilyaa@yandex.ru>
2024-02-03 15:11:52 +0300

    fix: add header animation

--- src/widgets/header/ui.tsx
+++ src/widgets/header/ui.tsx
@@ -1,13 +1,18 @@
+'use client';
+
 import {  Navbar,   NavbarBrand,   NavbarContent, NavbarItem, NavbarMenu, NavbarMenuItem, NavbarMenuToggle } from "@nextui-org/react";
 import { FC } from 'react';
 import { NavbarRow, NavbarMobile } from "@/src/features/site-navigation";
 import { ThemeSwitcher } from '@/src/features/theme-switcher';
 import { headerHeight } from "./lib/constants";
 import { LogoChurch } from "@/src/shared/ui";
+import { motion } from "framer-motion";
+
+const MotionNavbar = motion(Navbar)
 
 export const Header: FC = () => {
   return (
-      <Navbar maxWidth="full" height={`${headerHeight}px`} isBordered className="fixed">
+      <MotionNavbar initial={{y: -headerHeight}} animate={{y: 0}} maxWidth="full" height={`${headerHeight}px`} isBordered className="fixed">
         <NavbarBrand>
           <LogoChurch className='w-[20px] h-auto text-foreground' />
         </NavbarBrand>
@@ -24,6 +29,6 @@ export const Header: FC = () => {
         </NavbarContent>
 
         <NavbarMobile />
-      </Navbar>
+      </MotionNavbar>
   )
 };
