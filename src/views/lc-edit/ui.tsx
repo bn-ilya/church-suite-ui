ffd07e43188ff4afe75d608e3903868ded37ca44
bn-ilya <bn.ilyaa@yandex.ru>
2024-02-29 07:10:23 +0300

    fix: delete component

--- src/views/lc-edit/ui.tsx
+++ src/views/lc-edit/ui.tsx
@@ -1,11 +1,9 @@
-import { LcEditBody } from "@/src/widgets/lc-edit-body"
 import { LcEditIntro } from "@/src/widgets/lc-edit-intro"
 
 export const LcEdit = () => {
   return (
     <div className={`mt-[64px]`}>
       <LcEditIntro />
-      <LcEditBody />
     </div> 
   )
 }
