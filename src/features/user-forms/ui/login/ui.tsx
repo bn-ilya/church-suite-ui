ff5f63dae54a54571e18253c9aeb634aee2646c9
bn-ilya <bn.ilyaa@yandex.ru>
2024-03-02 01:18:52 +0300

    auto-correct phone input

--- src/features/user-forms/ui/login/ui.tsx
+++ src/features/user-forms/ui/login/ui.tsx
@@ -5,9 +5,7 @@ import { Input } from "../../components/input/ui"
 import { useForm } from "react-hook-form";
 import { ILoginDataUser } from "@/src/shared/api";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-import { useAppDispatch } from "@/src/shared/model";
-import { setPhone } from "@/src/entities/user";
+import { FormEvent, useEffect, useState } from "react";
 import { useLoginOnSubmit } from "../../model/hooks/useLoginOnSubmit";
 import { ErrorHandler, ErrorModal } from "@/src/shared/ui";
 
@@ -15,6 +13,12 @@ export const UserLoginForm = () => {
   const {onSubmit, isLoading, errorInfo, data} = useLoginOnSubmit()
   const {register, handleSubmit, formState: {errors}} = useForm<ILoginDataUser>({mode: "onChange"});
   const router = useRouter();
+  const [phone, setPhone] = useState('');
+
+  const handleChangePhone = (e: FormEvent<HTMLInputElement>) => {
+    const onlyNumber = e.currentTarget.value.match(/\d+/g)?.join("");
+    setPhone(onlyNumber || '');
+  } 
 
   useEffect(()=>{
     if (data) {
@@ -31,9 +35,10 @@ export const UserLoginForm = () => {
             <span className="text-[12px] text-zinc-400">+7</span>
           }
           isRequired
-          {...register("phone", {required: "Заполниие телефон", pattern: {value: /^[0-9+-]+$/, message: "Используются недопустимые символы"}, minLength: {value: 10, message: "Недостаточно символов. Требуется 10"}, maxLength: {value: 10, message: "Лишние символы. Требуется 10"}})}
+          {...register("phone", {required: "Заполниие телефон", onChange: handleChangePhone, pattern: {value: /^[0-9+-]+$/, message: "Используются недопустимые символы"}, minLength: {value: 10, message: "Недостаточно символов. Требуется 10"}, maxLength: {value: 10, message: "Лишние символы. Требуется 10"}})}
           isInvalid={!!errors?.phone}
           errorMessage={errors?.phone?.message}
+          value={phone}
           type="tel"
           label="Номер телефона"
           placeholder="(xxx) xxx-xx-xx"
--- src/features/user-forms/ui/register/ui.tsx
+++ src/features/user-forms/ui/register/ui.tsx
@@ -7,7 +7,7 @@ import { IRegDataUser } from "@/src/shared/api";
 import { useCreateOnSubmit } from "../../model/hooks/useCreateOnSubmit";
 import { ErrorModal } from "@/src/shared/ui";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import { useAppDispatch } from "@/src/shared/model";
 import { setPhone } from "@/src/entities/user";
 
@@ -15,7 +15,12 @@ export const UserRegisterForm = () => {
   const {onSubmit, isLoading, errorMsg, data} = useCreateOnSubmit()
   const {register, handleSubmit, formState: {errors}} = useForm<IRegDataUser>({mode: "onChange"});
   const router = useRouter();
-  const dispatch = useAppDispatch();
+  const [phone, setPhone] = useState('');
+
+  const handleChangePhone = (e: FormEvent<HTMLInputElement>) => {
+    const onlyNumber = e.currentTarget.value.match(/\d+/g)?.join("");
+    setPhone(onlyNumber || '');
+  } 
 
   useEffect(()=>{
     if (data) {
@@ -41,9 +46,10 @@ export const UserRegisterForm = () => {
           <span className="text-[12px] text-zinc-400">+7</span>
         }
           isRequired
-          {...register("phone", {required: "Заполниие телефон", pattern: {value: /^[0-9+-]+$/, message: "Используются недопустимые символы"}, minLength: {value: 10, message: "Недостаточно символов. Требуется 10"}, maxLength: {value: 10, message: "Лишние символы. Требуется 10"}})}
+          {...register("phone", {required: "Заполниие телефон", onChange: handleChangePhone, pattern: {value: /^[0-9+-]+$/, message: "Используются недопустимые символы"}, minLength: {value: 10, message: "Недостаточно символов. Требуется 10"}, maxLength: {value: 10, message: "Лишние символы. Требуется 10"}})}
           isInvalid={!!errors?.phone}
           errorMessage={errors?.phone?.message}
+          value={phone}
           type="tel"
           label="Номер телефона"
           placeholder="(xxx) xxx-xx-xx"
