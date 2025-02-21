0aa95812fe6101bbd31dbd27f73ae012847761bd
Бондаренко Илья <bilm@mts.ru>
2025-02-21 23:36:56 +0300

    feat: стилизация под 2025 год

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,40 @@
 // tailwind.config.js
-import {heroui} from "@heroui/react";
+import { heroui } from "@heroui/react";
 import { Config } from "tailwindcss/types/config";
 
 /** @type {import('tailwindcss').Config} */
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   darkMode: "class",
-  plugins: [heroui()]
-}
+  plugins: [
+    heroui({
+      themes: {
+        dark: {
+          colors: {
+            primary: {
+              DEFAULT: "#23B39D",
+            },
+          },
+        },
+        light: {
+          colors: {
+            primary: {
+              DEFAULT: "#23B39D",
+            },
+          },
+        },
+      },
+    }),
+  ],
+};
 
 export default config;
