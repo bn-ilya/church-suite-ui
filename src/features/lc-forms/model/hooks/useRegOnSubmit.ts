5cb19c7cf73628216782f1629a62128f5af6a459
Бондаренко Илья <bilm@mts.ru>
2025-02-21 17:13:31 +0300

    fix: исправлено значение count

--- src/features/lc-forms/model/hooks/useRegOnSubmit.ts
+++ src/features/lc-forms/model/hooks/useRegOnSubmit.ts
@@ -1,52 +1,80 @@
 import { SubmitHandler } from "react-hook-form";
 import { FormDataToSend } from "../type";
-import { ILiveChatClient, useAddLiveChatClientChildrenBulkMutation, useAddLiveChatClientMutation, useSetLcFormMutation, useUploadImageMutation } from "@/src/shared/api";
+import {
+  ILiveChatClient,
+  useAddLiveChatClientChildrenBulkMutation,
+  useAddLiveChatClientMutation,
+  useSetLcFormMutation,
+  useUploadImageMutation,
+} from "@/src/shared/api";
 import { useEffect, useState } from "react";
 import { useErrorReq } from "@/src/shared/model";
 
+const countUsersByDefault = 1;
+
 export const useRegOnSubmit = () => {
-  const [uploadImage, {isLoading: isUploadedImage, error: errorUpload}] = useUploadImageMutation();
-  const [addLiveChatClient, {isLoading: isAddingClient, data, error: errorAddLc}] = useAddLiveChatClientMutation();
-  const [addLiveChatClientChildrenBulk, {isLoading: isAddingClientChildrens, data: dataChildrens, error: errorAddLcc}] = useAddLiveChatClientChildrenBulkMutation();
-  const [setLcForm, {isLoading: isLoadingSetLcForm, error: errorSetLc}] = useSetLcFormMutation()
+  const [uploadImage, { isLoading: isUploadedImage, error: errorUpload }] =
+    useUploadImageMutation();
+  const [
+    addLiveChatClient,
+    { isLoading: isAddingClient, data, error: errorAddLc },
+  ] = useAddLiveChatClientMutation();
+  const [
+    addLiveChatClientChildrenBulk,
+    {
+      isLoading: isAddingClientChildrens,
+      data: dataChildrens,
+      error: errorAddLcc,
+    },
+  ] = useAddLiveChatClientChildrenBulkMutation();
+  const [setLcForm, { isLoading: isLoadingSetLcForm, error: errorSetLc }] =
+    useSetLcFormMutation();
   const [isSuccess, setSuccess] = useState(false);
   const [isLoading, setLoading] = useState(false);
   const errorInfoUpload = useErrorReq(errorUpload);
   const errorInfoAddLc = useErrorReq(errorAddLc);
   const errorInfoSetLc = useErrorReq(errorSetLc);
-  
-  const onSubmit: SubmitHandler<FormDataToSend> = async (formData) => { 
-    if (!formData.count) {
-      formData.count = 1;
-    } 
 
-    const {files, childrens, ...liveChatClientData} = formData
+  const onSubmit: SubmitHandler<FormDataToSend> = async (formData) => {
+    if (!formData.count) {
+      formData.count = countUsersByDefault;
+      if (formData.childrens?.length) {
+        formData.count = countUsersByDefault + formData.childrens?.length;
+      }
+    }
+
+    const { files, childrens, ...liveChatClientData } = formData;
     const data: ILiveChatClient = liveChatClientData;
 
     if (files?.length) {
       const res = await uploadImage(files).unwrap();
-      data.cheques = res.map(file => file.id);
+      data.cheques = res.map((file) => file.id);
     }
 
-    console.log(childrens)
     if (childrens?.length) {
-      const dataChildrens = childrens.map(child => ({name: child}));
+      const dataChildrens = childrens.map((child) => ({ name: child }));
       const res = await addLiveChatClientChildrenBulk(dataChildrens).unwrap();
-      data.live_chat_client_childrens = res.data.map(child => child.id);
+      data.live_chat_client_childrens = res.data.map((child) => child.id);
     }
 
     const response = await addLiveChatClient(data).unwrap();
-    await setLcForm({lcFormId: response.data.id});
-    setSuccess(true)
+    await setLcForm({ lcFormId: response.data.id });
+    setSuccess(true);
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     if (isUploadedImage || isAddingClient || isLoadingSetLcForm) {
       setLoading(true);
     } else {
       setLoading(false);
     }
-  }, [isUploadedImage, isAddingClient, isLoadingSetLcForm])
+  }, [isUploadedImage, isAddingClient, isLoadingSetLcForm]);
 
-  return {onSubmit, isSuccess, isLoading, errors: [errorInfoUpload, errorInfoAddLc, errorInfoSetLc], data};
-}
+  return {
+    onSubmit,
+    isSuccess,
+    isLoading,
+    errors: [errorInfoUpload, errorInfoAddLc, errorInfoSetLc],
+    data,
+  };
+};
