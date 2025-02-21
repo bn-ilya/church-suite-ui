7f84ae9dc93f4a0303d8eb5a861c185d7acba4de
Бондаренко Илья <bilm@mts.ru>
2025-02-22 01:15:09 +0300

    refactor: временный пуш адреса прода для api

--- next.config.js
+++ next.config.js
@@ -7,50 +7,50 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: '/',
-        destination: '/livechat/register/1',
+        source: "/",
+        destination: "/livechat/register/1",
         permanent: true,
       },
       {
-        source: '/events/',
-        destination: '/livechat/register/1',
+        source: "/events/",
+        destination: "/livechat/register/1",
         permanent: true,
       },
       {
-        source: '/calendar/',
-        destination: '/livechat/register/1',
+        source: "/calendar/",
+        destination: "/livechat/register/1",
         permanent: true,
       },
       {
-        source: '/livechat/register/',
-        destination: '/livechat/register/1',
+        source: "/livechat/register/",
+        destination: "/livechat/register/1",
         permanent: true,
       },
       {
-        source: '/livechat/register',
-        destination: '/livechat/register/1',
+        source: "/livechat/register",
+        destination: "/livechat/register/1",
         permanent: true,
       },
       {
-        source: '/livechat/login/',
-        destination: '/livechat/login/1',
+        source: "/livechat/login/",
+        destination: "/livechat/login/1",
         permanent: true,
       },
       {
-        source: '/livechat/login',
-        destination: '/livechat/login/1',
+        source: "/livechat/login",
+        destination: "/livechat/login/1",
         permanent: true,
       },
-    ]
+    ];
   },
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
-        destination: "http://192.168.0.97:1337/api/:path*",
+        source: "/api/:path*",
+        destination: "https://admin-lc.church-krop.ru/api/:path*",
       },
-    ]
+    ];
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
