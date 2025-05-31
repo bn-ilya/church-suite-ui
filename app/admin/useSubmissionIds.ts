7e3f01f73bd891a0c3b70bff5bb9f061f3f005b2
Бондаренко Илья <bilm@mts.ru>
2025-05-31 23:09:46 +0300

    fix: пагинация + сортировка

--- app/admin/useSubmissionIds.ts
+++ app/admin/useSubmissionIds.ts
@@ -30,7 +30,8 @@ export const useSubmissionIds = () => {
         return;
       }
 
-      let url = `${process.env.NEXT_PUBLIC_FORMIO_BASE_URL}form/${process.env.NEXT_PUBLIC_FORMIO_FORM_ID}/submission`;
+      // Базовый URL с параметрами для получения всех подписок и сортировки по дате создания (сначала новые)
+      let url = `${process.env.NEXT_PUBLIC_FORMIO_BASE_URL}form/${process.env.NEXT_PUBLIC_FORMIO_FORM_ID}/submission?limit=1000&sort=-created`;
 
       // Добавляем параметры поиска, если они есть
       if (params && params.field && params.value) {
@@ -50,15 +51,15 @@ export const useSubmissionIds = () => {
             // Формируем регулярное выражение для поиска по части строки (регистронезависимый)
             // /value/i - i означает регистронезависимый поиск
             fieldPath += `__regex=/^.*${params.value}.*$/i`;
-            url += `?${fieldPath}`;
+            url += `&${fieldPath}`;
           } else {
             // Для других операторов используем синтаксис с двойным подчеркиванием
             fieldPath += `__${params.operator}`;
-            url += `?${fieldPath}=${encodeURIComponent(params.value)}`;
+            url += `&${fieldPath}=${encodeURIComponent(params.value)}`;
           }
         } else {
           // Для обычного равенства используем стандартный синтаксис
-          url += `?${fieldPath}=${encodeURIComponent(params.value)}`;
+          url += `&${fieldPath}=${encodeURIComponent(params.value)}`;
         }
       }
 
