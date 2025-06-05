b49f7b97063627b6f26969edfb9c4d18c50ef1e9
Бондаренко Илья <bilm@mts.ru>
2025-06-05 20:42:57 +0300

    fix: исправлена опечатка

--- src/features/profile-form/components/profile-user-intro/ui.tsx
+++ src/features/profile-form/components/profile-user-intro/ui.tsx
@@ -6,7 +6,7 @@ export const ProfileUserIntro = () => {
       <div className="max-w-xl w-full mx-auto px-6 pt-16 pb-4 flex flex-col items-center gap-4">
         <img src="/svg/logo.svg" alt="logo" />
         <h1 className="text-2xl text-center font-bold">
-          Редатирование регистрации
+          Редактирование регистрации
         </h1>
         <Divider className="my-2" />
       </div>
